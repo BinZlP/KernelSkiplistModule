61c422d82ca55636d565a67f7cc0b0ca7a49564f
Hyungseok Han <dfafqfdaes159@gmail.com>
2021-12-16 10:27:00 +0000

    Add common codes for skiplist

--- multi_skiplist.c
+++ multi_skiplist.c
@@ -19,8 +19,6 @@
 #include <linux/string.h>
 #include <linux/random.h>
 
-#include "fc_memory.h"
-#include "fast_mblock.h"
 #include "multi_skiplist.h"
 
 int multi_skiplist_init_ex(MultiSkiplist *sl, const int level_count,
